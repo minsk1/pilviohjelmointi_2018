c140d731603ddd17ebe1978e45409baa138a4361
Minna Juuma <minna.juuma@metropolia.fi>
2018-08-21 09:19:08 +0000

    Tehtäväsarja 1 tehty

--- w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
@@ -1,12 +1,20 @@
 function multiplyBy()
 {
 // check the input
-    
+    var number1 = document.getElementById("firstNumber").value;
+    var number2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").textContent = number1 * number2;
 }
 
 function divideBy() 
 { 
 // check the input
+    var number1 = document.getElementById("firstNumber").value;
+    var number2 = document.getElementById("secondNumber").value;
+    if (number1 != 0 && number2 != 0)  {
+        document.getElementById("result").textContent = number1 / number2;
+    }else {
+        document.getElementById("result").textContent = "ÄÄÄÄ ÄLÄ JAA NOLLALLA ÄÄÄÄÄÄ";
+    }
  
 }
-
--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,39 @@
 let targetNumber = Math.floor(Math.random() * 10) + 1;
+let guesses = 1;
 
 function init () {
-	console.log("You have to code this!");
+    console.log(targetNumber);
+console.log("You have to code this!");
+document.getElementById("submitBtn").onclick = function(){
+   var number = document.getElementById("number").value;
+   check(number);
+};
 }
 
 function check (value) {
-  
+  console.log("Checking...");
+  if (value == targetNumber) {
+      showWin();
+  }else if(value != targetNumber && guesses > 4){
+      showLoss();
+  }else if (value != targetNumber){
+      showError();
+      guesses++;
+  }
 }
 
 function showWin () {
-  
+  console.log("You Win!");
 }
 
 function showError () {
-  
+  console.log("Wrong Number!");
 }
 
 function showLoss () {
-  
+  console.log("You Lose!");
 }
 
-init();
+window.onload = function(){
+    init();
+}
